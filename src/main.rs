8484c3498f4914dad2f56054def8aba320124a95
NastyGamer <jones.m.987654321@gmail.com>
2021-01-02 20:32:07 +0100

    Full rework. Rust actually starts to look relatively nice now.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,55 @@
 extern crate reqwest;
 extern crate text_io;
-extern crate git2;
 
-use git2::Repository;
-use std::{fs, io};
-use text_io::read;
+use std::fs;
+use std::io;
 use std::path::PathBuf;
+use std::process::Command;
+use std::str::from_utf8;
+
+use text_io::read;
+
+const DEBUG: bool = true;
 
 fn main() -> io::Result<()> {
-    let subject_folders = list_subject_folders();
-    for folder in subject_folders.clone() {
-        println!("{}", folder)
+    match Command::new("git").arg("--version").output() {
+        Ok(v) => { println!("{}", from_utf8(&*v.stdout).unwrap()) }
+        Err(_) => { panic!("Git not found") }
     }
-    println!("Select Subject:");
+    let subject_folders = list_subject_folders();
+    if !subject_folders.contains(&String::from(".git")) {
+        println!("Preparing workspace...");
+        Command::new("git")
+            .arg("init")
+            .current_dir(".")
+            .output()
+            .expect("Unable to prepare workspace");
+    }
+    println!("Available subjects:");
+    for folder in &subject_folders {
+        println!("\t{}", folder);
+    }
     let mut valid_subject_choice: bool = false;
     let mut subject_choice: String = String::from("");
     while !valid_subject_choice {
+        println!("Select subject:");
         subject_choice = read!();
         if subject_folders.contains(&subject_choice) {
             valid_subject_choice = true;
         } else {
-            println!("{}", format!("{} doesn't exist. Create now? (y/n)", subject_choice));
-            let new_sub: String = read!();
-            if new_sub.to_lowercase().eq("y") {
-                println!("Creating new subject {}", subject_choice.clone());
-                create_new_repo(PathBuf::from(subject_choice.clone()));
+            println!("{} doesn't exist. Create it? (y/n)", &subject_choice);
+            let create_new_subject: String = read!();
+            if create_new_subject.to_lowercase().eq("y") {
+                println!("Creating new subject {}...", &subject_choice);
+                create_new_repo(PathBuf::from(&subject_choice));
                 valid_subject_choice = true;
             }
         }
     }
     println!("Enter Document name:");
     let document_choice: String = read!();
-    println!("Creating document {:?}", PathBuf::from(".").join(subject_choice.clone()).join(document_choice.clone()));
-    create_new_document(PathBuf::from(".").join(subject_choice.clone()).join(document_choice.clone()));
+    println!("Creating document {:?}...", PathBuf::from(".").join(&subject_choice).join(&document_choice));
+    create_new_document(PathBuf::from(".").join(&subject_choice).join(&document_choice));
     println!("Done!\nPress enter to continue...");
     let _: String = read!("{}\n");
     Ok(())
@@ -40,16 +57,40 @@ fn main() -> io::Result<()> {
 
 fn create_new_repo(folder: PathBuf) {
     println!("Creating new repo");
-    Repository::init(folder.clone()).expect("Unable to init new repository");
-    write_files(folder)
+    fs::create_dir(&folder).expect("Unable to create subject folder");
+    if DEBUG { println!("Running git init in {:?}", &folder); }
+    Command::new("git")
+        .arg("init")
+        .current_dir(&folder)
+        .output()
+        .expect("Unable to init new repo");
+    write_files(folder.clone());
+    if DEBUG { println!("Running git add * in {:?}", &folder); }
+    Command::new("git")
+        .args(&["add", "*"])
+        .current_dir(&folder)
+        .output()
+        .expect("Unable to add files for commit");
+    if DEBUG { println!("Running git commit -m \"Initial commit\" in {:?}", &folder); }
+    Command::new("git")
+        .args(&["commit", "-m", "Initial commit"])
+        .current_dir(&folder)
+        .output()
+        .expect("Unable to commit changes");
+    if DEBUG { println!("Running git submodule add ./{} in .", &folder.file_name().unwrap().to_str().unwrap()); }
+    Command::new("git")
+        .args(&["submodule", "add", &(String::from("./") + &folder.file_name().unwrap().to_str().unwrap())])
+        .current_dir(".")
+        .output()
+        .expect("Unable to add submodule");
 }
 
 fn create_new_document(folder: PathBuf) {
     println!("Downloading template");
-    fs::create_dir_all(folder.clone().join("src")).expect("Unable to create document folder");
-    fs::write(folder.clone().join("src").join("main.tex"), get("https://gist.githubusercontent.com/NastyGamer/722a29264a7d3bad7b0157097b9ec1b2/raw/df1753fcc8710ff5e40427345631cdc2f77e0df4/LaTeX-Template.tex".parse().unwrap())).expect("Unable to write template");
-    fs::write(folder.clone().join("src").join("solarized.sty"), get("https://raw.githubusercontent.com/jez/latex-solarized/master/solarized.sty".parse().unwrap())).expect("Unabel to write solarized style");
-    fs::write(folder.clone().join("src").join("solarized-dark.sty"), get("https://raw.githubusercontent.com/jez/latex-solarized/master/solarized-dark.sty".parse().unwrap())).expect("Unable to write solarized dark style");
+    fs::create_dir_all(folder.join("src")).expect("Unable to create document folder");
+    fs::write(folder.join("src").join("main.tex"), get("https://gist.githubusercontent.com/NastyGamer/722a29264a7d3bad7b0157097b9ec1b2/raw/df1753fcc8710ff5e40427345631cdc2f77e0df4/LaTeX-Template.tex".parse().unwrap())).expect("Unable to write template");
+    fs::write(folder.join("src").join("solarized.sty"), get("https://raw.githubusercontent.com/jez/latex-solarized/master/solarized.sty".parse().unwrap())).expect("Unabel to write solarized style");
+    fs::write(folder.join("src").join("solarized-dark.sty"), get("https://raw.githubusercontent.com/jez/latex-solarized/master/solarized-dark.sty".parse().unwrap())).expect("Unable to write solarized dark style");
 }
 
 fn list_subject_folders() -> Vec<String> {
